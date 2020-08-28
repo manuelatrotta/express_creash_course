c4346bfd88cf6c5d992bd9ed9913d3f4a18160ae
manuelatrotta <manuelat88@hotmail.it>
2020-08-28 17:00:10 +0200

    first node.js app

---
+++ Members.js
@@ -0,0 +1,29 @@
+const members = [
+    {
+        id:1,
+        name: 'John Winchester',
+        email: 'jonny@imail.com',
+        status : 'active'
+    },
+    {
+        id:2,
+        name: 'Willy Orso',
+        email: 'orso@imail.com',
+        status : 'active'
+    },
+    {
+        id:3,
+        name: 'Milligan Sergente',
+        email: 'mill@imail.com',
+        status : 'active'
+    },
+    {
+        id:4,
+        name: 'Soldato gatto',
+        email: 'sold@imail.com',
+        status : 'active'
+    }
+]
+
+
+module.exports = members;
