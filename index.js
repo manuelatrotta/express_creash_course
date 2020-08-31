8051fd9a83a2cec44c887e10ee432c964f610397
manuelatrotta <manuelat88@hotmail.it>
2020-08-31 07:44:32 +0200

    verifica id

--- index.js
+++ index.js
@@ -8,16 +8,24 @@ const app = express();
 app.use(logger);
 
 // gets all members
-app.get('/api/members', (req, res) => 
+app.get('/api/members', (req, res) =>
     res.json(members));
 
-// get a single member with id 
+// get a single member with id
 
 app.get('/api/members/:id', (req, res) => {
+
+  const found = members.some(member => member.id === parseInt(req.params.id));
+
+
+  if(found) {
     res.json(members.filter(member => member.id === parseInt(req.params.id)));
+  }else {
+    res.status(400).json ({msg:'member not found'});
+  }
 });
 
-//set static folder 
+//set static folder
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -25,4 +33,3 @@ app.use(express.static(path.join(__dirname, 'public')));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=> console.log('Server started at 5000'));
-
