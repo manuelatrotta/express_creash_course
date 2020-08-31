6470cdb166c50d4726f879902c62aeafba6b2483
manuelatrotta <manuelat88@hotmail.it>
2020-08-31 08:54:23 +0200

    insert update

--- routes/api/members.js
+++ routes/api/members.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const uuid = require('uuid');
 const members = require('../../Members');
 
 
@@ -23,7 +24,39 @@ router.get('/:id', (req, res) => {
 
 // create a member
 router.post('/', (req, res) => {
-  res.send(req.body);
+  //res.send(req.body); prova su postman
+  const newMember = {
+    id : uuid.v4(),
+    name : req.body.name,
+    email : req.body.email,
+    status :'active'
+  }
+  if(!newMember.name || !newMember.email) {
+    return res.status(400).json({msg : 'Please insert a name and email'});
+  }
+  members.push(newMember);
+  res.json(members);
+});
+
+//update member
+
+router.put('/:id', (req, res) =>{
+  const found = members.some(member => member.id === parseInt(req.params.id));
+
+  if(found) {
+    const updMember = req.body;
+    members.forEach(member => {
+      if(member.id === parseInt(req.params.id)) {
+        member.name = updMember.name ? updMember.name : member.name;
+        member.email = updMember.email ? updMember.email : member.email;
+
+        res.json({msg: 'member update', member})
+      }
+    })
+  }else {
+    res.status(400).json ({msg: `member wit id: ${req.params.id} not found`});
+  }
+
 })
 
 
